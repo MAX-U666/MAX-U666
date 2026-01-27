8e546e425cd988e6efcbedb3ee903312d2060d02
MAX-U666 <officexu123@gmail.com>
2026-01-27 15:53:43 +0800

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { GMVLogo, MiniLogo } from './Logo';
+export { default as Header } from './Header';
+export { default as DayTable } from './DayTable';
+export { NewProductModal, UploadModal, AbnormalModal } from './Modals';
