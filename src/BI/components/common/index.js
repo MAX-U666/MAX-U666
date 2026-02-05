348b3e82a5008f6d3d688116ce2d2808333bb3f6
MAX-U666 <officexu123@gmail.com>
2026-02-05 14:46:31 +0800

    [BI] Add common components index

---
+++ src/BI/components/common/index.js
@@ -0,0 +1,7 @@
+/**
+ * BI 通用组件统一导出
+ */
+export { CostBar } from './CostBar';
+export { MetricCard } from './MetricCard';
+export { SummaryCard } from './SummaryCard';
+export { TrendCard } from './TrendCard';
