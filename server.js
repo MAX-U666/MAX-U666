6400e30a646a01cb4fb3ff7a48d423f4be6b8699
MAX-U666 <officexu123@gmail.com>
2026-02-06 15:59:55 +0800

    fix: 加大JSON body限制到50mb

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // 数据库连接池
 const pool = mysql.createPool({
